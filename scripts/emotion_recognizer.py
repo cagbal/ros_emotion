b32cca1f1eb575fbda708af2a4be9d2bd79c8fc2
cagbal <cagatayodabasi91@gmail.com>
2018-11-15 10:53:19 +0100

    Removed Cv Bridge

--- scripts/emotion_recognizer.py
+++ scripts/emotion_recognizer.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python
-import os
 
 import rospy
-from cv_bridge import CvBridge, CvBridgeError
+from rospy.numpy_msg import numpy_msg
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+
+import cv2
+import dlib
+import numpy as np
 
 from EmoPy.src.fermodel import FERModel
 
 class EmotionRecognizer(object):
 
-    def __init__(self, emotions=['calm', 'anger', 'happiness']):
-        self.sub = rospy.Subscriber('camera/image_raw', Image, self.recognize)
-        self.pub = rospy.Publisher('emotion_image', Image, queue_size=1)
+    def __init__(self, emotions=['disgust', 'happiness', 'surprise', ]):
+        self.sub = rospy.Subscriber('/body_tracker/rgb/raw_image', Image, self.recognize)
+        self.pub = rospy.Publisher('emotion_image', numpy_msg(Image), queue_size=0)
         self.count = 0
-        self.bridge = CvBridge()
         self.target_emotions = emotions
+        self.detector = dlib.get_frontal_face_detector()
+        self.model = FERModel(self.target_emotions, verbose=True)
 
     def recognize(self, msg):
-        self.count += 1
-        if self.count % 5 != 0:
-            self.pub.publish(msg)
-            return
         try:
-            frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-            faces = emopy.get_faces(frame) # faces: [(left, top, right, bottom), (...)]
-            if not faces:
-                return
-            biggest_face = max(faces, key=lambda rect: (rect[1][0]-rect[0][0])*(rect[1][1]-rect[0][1]))
-            emotions = emopy.recognize(frame, [biggest_face])
-            frame = emopy.overlay(frame, [biggest_face], emotions)
-            emotion = emotions[0]
-            ros_frame = self.bridge.cv2_to_imgmsg(frame, "bgr8")
-            self.pub.publish(ros_frame)
-        except CvBridgeError as ex:
-            print(ex)
+            frame = np.frombuffer(
+                msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+
+            dets = self.detector(frame, 1)
+
+            assert len(dets) > 0, "There is no face!"
+
+            for d in dets:
+                self.model.predict(
+                    frame[d.top():d.bottom(), d.left():d.right()])
+                cv2.imshow("a", frame[d.top():d.bottom(), d.left():d.right()])
+                cv2.waitKey(1)
+            #self.pub.publish(ros_frame)
+                break
+        except Exception as e:
+            print("Error: " + str(e))
 
 if __name__ == '__main__':
     rospy.init_node('emotion')
